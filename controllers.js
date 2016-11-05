2446d66bc13d141d839ec2722a49198a7aa40244
Colin But <colinbut@users.noreply.github.com>
2016-11-05 21:09:12 +0000

    added the customer controller

---
+++ controllers.js
@@ -0,0 +1,5 @@
+customerControllers.controller('CustomerController', ['$scope',
+  function CustomerController($scope) {
+    $scope.customerName = "Joe";
+    $scope.customerNumber = "01316571234";
+  }]);
