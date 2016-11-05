081092f67d3dd6d43ac931ccbf3e17a6e60d5340
Colin But <colinbut@users.noreply.github.com>
2016-11-05 21:22:37 +0000

    allowing change the customer details - editting

--- controllers.js
+++ controllers.js
@@ -1,5 +1,14 @@
 customerControllers.controller('CustomerController', ['$scope',
   function CustomerController($scope) {
+
+    // initialise some data
     $scope.customerName = "Joe";
     $scope.customerNumber = "01316571234";
+
+    // change the data
+    $scope.changeCustomer = function() {
+      $scope.customerName = $scope.cName;
+      $scope.customerNumber = $scope.cNumber;
+    }
+
   }]);
